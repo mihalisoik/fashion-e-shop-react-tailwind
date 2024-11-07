45ffe9acab236cc0e192ec8ae5605aeac10cedbd
Michael <mihalisoik@gmail.com>
2024-11-07 14:03:57 +0200

    stage1 of trying to pass the animatedTooltip in my components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CustomerReviews from "./sections/CustomerReviews";
 import Footer from "./sections/Footer";
 import Hero from "./sections/Hero";
@@ -6,9 +7,19 @@ import Newsletter from "./sections/Newsletter";
 import PopularItems from "./sections/PopularItems";
 
 export default function App() {
+
+  const [renderTooltip,setRenderTooltip] = useState(false)
+
+  function animatedTooltip() {
+    setRenderTooltip(true)
+    setTimeout(()=>{
+      setRenderTooltip(false)
+    },2500)
+  }
+
   return (
     <div>
-      <Navbar />
+      <Navbar renderTooltip={renderTooltip}/>
       <Hero />
       <PopularItems />
       <CustomerReviews />
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,21 +1,8 @@
-import { useState } from "react"
+function Navbar({renderTooltip}) {
 
-function Navbar() {
-
-  const [renderTooltip,setRenderTooltip] = useState(false)
-
-  function animatedTooltip() {
-    setRenderTooltip(true)
-    setTimeout(()=>{
-      setRenderTooltip(false)
-    },2500)
-  }
 
   return (
     <nav className="px-5 max-sm:px-1 bg-accent fixed left-0 right-0 top-0 h-16 z-10 grid grid-cols-[auto,1fr] items-center">
-      <button onClick={animatedTooltip}>
-        yes
-      </button>
       <img src="src/assets/icons/menu.svg" alt="menu-icon"/>
       <div className='flex items-center justify-between sm:ml-3'>
       <a href="index.html" className="flex items-center">
@@ -33,7 +20,7 @@ function Navbar() {
           <p>Your Cart: </p>
           <img src="src/assets/icons/cart.svg" alt="cart" />
           {renderTooltip && <div className="absolute top-7 right-0 whitespace-nowrap bg-gray-300/60 text-sm py-1 px-3 rounded-md goupstyle">
-            Added to Cart
+           Added to Cart
           </div>}
        </div>
       </div>
