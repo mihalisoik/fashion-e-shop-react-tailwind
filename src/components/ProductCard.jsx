64b05ab4d9ac21244f2545f804b329572eb9229f
Michael <mihalisoik@gmail.com>
2024-11-22 12:26:46 +0200

    made the out of stock button for my productcard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,44 +16,58 @@ function ProductCard({
   animatedAddedTooltip,
   animatedRemovedTooltip,
 }) {
-  const [selectedSize, setSelectedSize] = useState(Object.keys(stock)[0]);
   const [productQuantity, setProductQuantity] = useState(0);
-  const isInCart = productQuantity > 0;
   const [favorited, setFavorited] = useState(isFavorite);
+  const [selectedSize, setSelectedSize] = useState(Object.keys(stock)[0]);
+  const [stockQuantity, setStockQuantity] = useState(Object.values(stock)[0]);
+  const [isTrembling, setIsTrembling] = useState(false);
+
+  const isInCart = productQuantity > 0;
 
   useEffect(() => {
     const currentItem = cart.find(
       (item) => item.id === id && item.size === selectedSize
     );
     setProductQuantity(currentItem ? currentItem.quantity : 0);
-  }, [cart, id, selectedSize]);
+  }, [selectedSize]);
 
-  const handleSizeChange = (event) => {
+  function handleSizeChange(event) {
+    const selectedOption = event.target.options[event.target.selectedIndex];
+    setStockQuantity(Number(selectedOption.dataset.quantity));
     setSelectedSize(event.target.value);
-  };
+  }
 
-  const handleAddToCart = () => {
+  function handleAddToCart() {
     animatedAddedTooltip();
     setRenderTotalQuantity((oldValue) => oldValue + 1);
     setProductQuantity((oldValue) => oldValue + 1);
     addToCart(id, selectedSize);
-  };
+  }
 
-  const handleDecrement = () => {
+  function handleDecrement() {
     animatedRemovedTooltip();
     setRenderTotalQuantity((oldValue) => oldValue - 1);
     setProductQuantity((oldValue) => oldValue - 1);
     removeFromCart(id, selectedSize);
-  };
+  }
 
-  const handleToggleFavorite = () => {
+  function handleToggleFavorite() {
     clothes.forEach((clothe) => {
       if (clothe.id === id) {
         clothe.isFavorite = !clothe.isFavorite;
         setFavorited(clothe.isFavorite);
       }
     });
-  };
+  }
+
+  function handleTrembleClick() {
+    setIsTrembling(true);
+
+    setTimeout(() => {
+      setIsTrembling(false);
+    }, 500);
+  }
+  //na ftiaksw to outofstock
 
   return (
     <div className="bg-slate-100 p-5 font-secondary flex flex-col gap-1 justify-center items-start rounded-md shadow-lg flex-shrink-0 w-52 text-sm">
@@ -69,30 +83,29 @@ function ProductCard({
       </button>
       <Link to={`/item?id=${encodeURIComponent(id)}`} key={id}>
         <img src={image} alt={name} width={150} />
-        <div className="flex w-full font-secondary justify-between items-center">
-          <div className="flex items-center gap-0.5">
-            {(rating.stars * 10) % 10 === 0 ? (
-              <img src="src/assets/icons/star.svg" alt="star-rating" />
-            ) : (
-              <img
-                src="src/assets/icons/star-half.svg"
-                alt="half-star-rating"
-              />
-            )}
-            <p>{rating.stars.toFixed(1)}</p>
-          </div>
-          <select
-            className="bg-accent border border-1 border-gray-500 text-slate-50 rounded-full px-1 py-0.5"
-            onChange={handleSizeChange}
-            value={selectedSize}
-          >
-            {Object.entries(stock).map(([size, quantity]) => (
-              <option key={size} value={size} data-quantity={quantity}>
-                {size}
-              </option>
-            ))}
-          </select>
+      </Link>
+      <div className="flex w-full font-secondary justify-between items-center">
+        <div className="flex items-center gap-0.5">
+          {(rating.stars * 10) % 10 === 0 ? (
+            <img src="src/assets/icons/star.svg" alt="star-rating" />
+          ) : (
+            <img src="src/assets/icons/star-half.svg" alt="half-star-rating" />
+          )}
+          <p>{rating.stars.toFixed(1)}</p>
         </div>
+        <select
+          className="bg-accent border border-1 border-gray-500 text-slate-50 rounded-full px-1 py-0.5"
+          onChange={handleSizeChange}
+          value={selectedSize}
+        >
+          {Object.entries(stock).map(([size, quantity]) => (
+            <option key={size} value={size} data-quantity={quantity}>
+              {size}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Link to={`/item?id=${encodeURIComponent(id)}`} key={id}>
         <h3 className="font-secondary font-semibold">{name}</h3>
         <div className="font-secondary">
           <span className="text-[0.6rem]">€</span>
@@ -101,12 +114,25 @@ function ProductCard({
       </Link>
       <div className="mt-2">
         {!isInCart ? (
-          <button
-            className="flex w-40 h-10 gap-2 text-[#666666] text-sm px-3 py-2 bg-slate-50 rounded-3xl justify-center items-center font-semibold shadow-md hover:bg-slate-100 active:scale-95"
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-            <img src="src/assets/icons/add-to-cart.svg" alt="Add to Cart" />
+          <button className="group w-40 h-10 text-[#666666] text-sm px-3 py-2 bg-slate-50 rounded-3xl font-semibold shadow-md hover:bg-slate-100">
+            {stockQuantity > 0 ? (
+              <div
+                className="flex justify-center items-center gap-2 active:scale-95"
+                onClick={handleAddToCart}
+              >
+                <p>Add To Cart</p>
+                <img src="src/assets/icons/add-to-cart.svg" alt="Add to Cart" />
+              </div>
+            ) : (
+              <div
+                className={`text-red-600 cursor-not-allowed ${
+                  isTrembling ? "tremble" : ""
+                }`}
+                onClick={handleTrembleClick}
+              >
+                <p>Out Of Stock</p>
+              </div>
+            )}
           </button>
         ) : (
           <div className="flex w-40 h-10 text-[#666666] text-base px-3 py-2 bg-slate-50 rounded-3xl justify-between items-center shadow-md">
